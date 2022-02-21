0ef8b9a33768442c58b3793b0090485461969308
Thalukanyo Ramasindi <thaluramasindi@gmail.com>
2022-02-21 18:09:05 +0200

    Added click functionality on the attraction url

--- app/src/main/java/com/example/android/joburgtourguide/AttractionAdapter.java
+++ app/src/main/java/com/example/android/joburgtourguide/AttractionAdapter.java
@@ -1,6 +1,9 @@
 package com.example.android.joburgtourguide;
 
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +23,7 @@ public class AttractionAdapter extends ArrayAdapter<Attraction> {
     }
 
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent){
         View listView = convertView;
         if(listView == null) {
 
@@ -28,13 +31,22 @@ public class AttractionAdapter extends ArrayAdapter<Attraction> {
 
         }
 
-        Attraction currentAttraction = getItem(position);
+        final Attraction currentAttraction = getItem(position);
 
         TextView attractionName = (TextView) listView.findViewById(R.id.attraction_name_text_view);
         attractionName.setText(currentAttraction.getmAttractionName());
 
-        TextView website = (TextView) listView.findViewById(R.id.website_text_view);
+        final TextView website = (TextView) listView.findViewById(R.id.website_text_view);
         website.setText(currentAttraction.getmWebsite());
+        //TODO: make the link clickable
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://" + currentAttraction.getmWebsite()));
+                view.getContext().startActivity(intent);
+            }
+        });
 
         ImageView attractionImage = (ImageView) listView.findViewById(R.id.attractionImage);
         attractionImage.setImageResource(currentAttraction.getmImageResourceId());
@@ -42,5 +54,15 @@ public class AttractionAdapter extends ArrayAdapter<Attraction> {
 
 
         return listView;
+    }
+    private void setonClick(final TextView tv, final String link){
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(link));
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 }
